5e2a16e0ffb276c6a1b80e6f7fd1d136c2262b24
harshal-g25 <harshalgavali2545@gmail.com>
2026-03-14 01:44:17 +0530

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="landing-page">
+      <h1>Paradise Nursery</h1>
+      <p>Your one-stop shop for beautiful indoor and outdoor plants.</p>
+
+      <button className="get-started-btn">
+        Get Started
+      </button>
+    </div>
+  );
+}
+
+export default App;
